8a46ac43a56bf3d4c8090a09de055dcdb7d9457d
Martvie <marviedias@gmail.com>
2023-05-28 22:12:12 -0300

    adição de mensagem de erro na criação

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,40 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
             title: "asc",
         },
-        include:{
+        include: {
             genres: true,
-            languages: true
-        }
-    }
-    );
+            languages: true,
+        },
+    });
     res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+    const { id, title, genre_id, language_id, oscar_count, release_date } =
+        req.body;
+    try {
+        await prisma.movie.create({
+            data: {
+                id,
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "falha ao cadastrar" });
+    }
+
+    res.status(201).send();
 });
 
 app.listen(port, () => {
