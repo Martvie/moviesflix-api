db4f4cf68864631ccbeffdc9c992b73b692f8176
Martvie <marviedias@gmail.com>
2023-06-12 16:10:27 -0300

    update de filme

--- src/server.ts
+++ src/server.ts
@@ -26,12 +26,14 @@ app.post("/movies", async (req, res) => {
     try {
         const movieWithTheSameTitle = await prisma.movie.findFirst({
             where: {
-                title: { equals: title, mode: "insensitive"}
+                title: { equals: title, mode: "insensitive" },
             },
         });
 
-        if(movieWithTheSameTitle){
-            return res.status(409).send({message: "Já exist um filme cadastrado com esse titulo."});
+        if (movieWithTheSameTitle) {
+            return res.status(409).send({
+                message: "Já exist um filme cadastrado com esse titulo.",
+            });
         }
 
         await prisma.movie.create({
@@ -49,6 +51,38 @@ app.post("/movies", async (req, res) => {
     }
 
     res.status(201).send();
+});
+
+app.put("/movies/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const movie = await prisma.movie.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined;
+
+        await prisma.movie.update({
+            where: {
+                id,
+            },
+            data: data,
+        });
+    } catch (error) {
+        res.status(500).send({ message: "Falha na atulização" });
+    }
+
+    res.status(200).send();
 });
 
 app.listen(port, () => {
