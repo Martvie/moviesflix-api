1c9a8fd836f4ff69ba7df86c50c5462acedf2def
Martvie <marviedias@gmail.com>
2023-06-13 23:31:51 -0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import SwaggerUi from "swagger-ui-express";
+import SwaggerDocument from "../swagger.json";
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use("/docs", SwaggerUi.serve, SwaggerUi.setup(SwaggerDocument));
 
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
